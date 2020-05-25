36b8f156107a866cdb77a27b22244b368290bca6
Poojan Dave <dave.sfsu@gmail.com>
2020-05-24 22:41:54 -0700

    Contacts Permissions: Fixed issues in Dart Analysis

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:contacts_service/contacts_service.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:app_settings/app_settings.dart';
 
